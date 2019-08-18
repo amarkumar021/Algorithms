cddd98ea4a46f53467d6bf57d21cec5fb643e458
Amar <amarkumar021@gmail.com>
2019-08-18 13:36:52 +0530

    minor change

--- RecursionAlgorithms/src/com/amar/learn/FactorialRecursion.java
+++ RecursionAlgorithms/src/com/amar/learn/FactorialRecursion.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class FactorialRecursion {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter a Number");
+        System.out.println("Enter a Number: ");
         int num = scanner.nextInt();
         System.out.println("Factorial of: " + num + " is " + factorial(num));
     }
